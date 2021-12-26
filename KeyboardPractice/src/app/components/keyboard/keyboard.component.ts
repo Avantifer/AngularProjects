2b5ac7340539fc211e6e6fad09fe9415c9973cf3
Fernando <ferenandoruiz@gmail.com>
2021-12-26 23:53:03 +0100

    Logic finished

--- KeyboardPractice/src/app/components/keyboard/keyboard.component.ts
+++ KeyboardPractice/src/app/components/keyboard/keyboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-keyboard',
@@ -12,4 +12,38 @@ export class KeyboardComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  class : string = 'keyboard__line__key__selected';
+
+  barra : string = '\\';
+  arrKeys : string[] = ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '=', 'backspace',
+                      'tab', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', '[', ']', '\\',
+                      'caps', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ';', "'", 'enter',
+                      'shiftleft', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/', 'shiftright']
+
+  @HostListener('document:keydown', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    let selected = document.querySelector('.' + this.class);
+    let newSelected : HTMLElement | null;
+
+    let clicked : boolean = false;
+    let randomKey = this.arrKeys[Math.round(Math.random() * this.arrKeys.length - 1)];
+    newSelected = document.getElementById(randomKey);
+
+    if (selected != null && newSelected != null) {
+      if (event.key === this.barra[0]){
+        clicked = true;
+      }else if (event.key.toLowerCase() === 'shift') {
+        if (event.code.toLowerCase() === selected.id) {
+          clicked = true;
+        }
+      }else if (selected.id === event.key.toLowerCase()) {    
+        clicked = true;
+      }
+
+      if (clicked) {
+        selected.classList.remove(this.class);
+        newSelected.classList.add(this.class);
+      }
+    }
+  }
 }
