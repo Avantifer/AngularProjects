58d6370b61c2bfafb13d7934e95941f2f7b93db1
Fernando <ferenandoruiz@gmail.com>
2021-12-28 15:03:51 +0100

    Simple structure of the project added

---
+++ TaskList/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { TaskListComponent } from './components/task-list/task-list.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskListComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
