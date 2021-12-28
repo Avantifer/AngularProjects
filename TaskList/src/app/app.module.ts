29b8dc342f85f38affad5832685665b4dbe06668
Fernando <ferenandoruiz@gmail.com>
2021-12-28 21:11:02 +0100

    Angular material added, functionality of tasklist finished

--- TaskList/src/app/app.module.ts
+++ TaskList/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './components/task-list/task-list.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,11 @@ import { TaskListComponent } from './components/task-list/task-list.component';
     TaskListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- TaskList/src/app/components/task-list/task-list.component.ts
+++ TaskList/src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-task-list',
@@ -7,9 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.tasks = this.fb.group({
+      firstTask : false,
+      secondTask : false,
+      thirdTask : false,
+      fourthTask : false,
+      fifthTask : false,
+      sixthTask : false,
+      seventhTask : false,
+      eighthTask : false,
+      ninthTask : false,
+      tenthTask : false
+    })
+
+    this.tasks.valueChanges.subscribe( (taskList) => {
+      for (let key in taskList) {
+        let taskSelected = document.getElementById(key);
+        if (taskSelected != null) {
+          if (taskList[key]) {
+            taskSelected.classList.add('checked');
+          } else {
+            if (taskSelected.classList.contains('checked')) {
+              taskSelected.classList.remove('checked');
+            }
+          }
+        }
+      }
+    })
   }
 
+  tasks: FormGroup = new FormGroup({})
 }
