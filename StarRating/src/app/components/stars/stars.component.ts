57d35a77958c125d9b1a9c5190fd83982dedc1ab
Fernando <ferenandoruiz@gmail.com>
2022-01-04 23:39:48 +0100

    Improve scss with a structure of files

--- StarRating/src/app/components/stars/stars.component.ts
+++ StarRating/src/app/components/stars/stars.component.ts
@@ -15,7 +15,7 @@ export class StarsComponent implements OnInit {
   changeStarYellow(event : any) {
     let postionStar = event.srcElement.classList[1].substr(event.srcElement.classList[1].length - 1);
     let allStars = document.querySelectorAll('.star');
-    
+
     allStars.forEach( (star) => {
       let allStarPosition = star.classList[1].substring(star.classList[1].length - 1);
       if (allStarPosition <= postionStar) {
@@ -27,10 +27,10 @@ export class StarsComponent implements OnInit {
   changeStarBlank(event : any) {
     let postionStar = event.srcElement.classList[1].substr(event.srcElement.classList[1].length - 1);
     let allStars = document.querySelectorAll('.star');
-    
+
     allStars.forEach( (star) => {
       let allStarPosition = star.classList[1].substring(star.classList[1].length - 1);
-      if (allStarPosition >= postionStar) {
+      if (allStarPosition > postionStar) {
         star.setAttribute('src', '../../../assets/images/star-blank.png');
       }
     });
