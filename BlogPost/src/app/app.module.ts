b8c9a28db2072a7ce28af25ee45fa1e583dc202c
Fernando <ferenandoruiz@gmail.com>
2022-01-06 01:07:18 +0100

    Project finished

--- BlogPost/src/app/app.module.ts
+++ BlogPost/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BlogComponent } from './components/blog/blog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- BlogPost/src/app/components/blog/blog.component.ts
+++ BlogPost/src/app/components/blog/blog.component.ts
@@ -12,4 +12,29 @@ export class BlogComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  contents = [
+    {'position' : 0, 'content' : 'My Story', 'link' : 'Story' },
+    {'position' : 1, 'content' : 'Priming', 'link' : 'Priming' },
+    {'position' : 2, 'content' : 'Enter Framer', 'link' : 'Framer' },
+    {'position' : 3, 'content' : 'For the no-coders. Framer is for you.', 'link' : 'No-coders' },
+    {'position' : 4, 'content' : 'For the code-savvy. Framer is for you.', 'link' : 'Code-savvy' },
+    {'position' : 5, 'content' : 'My Sweet Spot: How Our Team is Using Framer', 'link' : 'Spot' },
+    {'position' : 6, 'content' : 'Figma', 'link' : 'Figma' },
+    {'position' : 7, 'content' : 'My Framer Desktop', 'link' : 'Desktop' },
+    {'position' : 8, 'content' : 'Tight Developer Schedule', 'link' : 'Schedule'},
+  ];
+  
+  selectLink(event : any) {
+    let allLinks = document.querySelectorAll('.blog__nav__content');
+    allLinks.forEach( (link) => {
+      if (link.classList.contains('selected')) {
+        link.classList.remove('selected');
+      }
+    });
+
+    if (event.srcElement.localName === 'a') {
+      event.path[1].classList.add('selected');
+    }
+    event.srcElement.classList.add('selected');
+  }
 }
