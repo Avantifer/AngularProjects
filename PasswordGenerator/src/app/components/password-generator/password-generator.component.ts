cfa99ab940c4b5571427e17907c8632c7d3b50ce
Fernando <ferenandoruiz@gmail.com>
2022-01-06 13:40:36 +0100

    Simple structure of the project added

---
+++ PasswordGenerator/src/app/components/password-generator/password-generator.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-password-generator',
+  templateUrl: './password-generator.component.html',
+  styleUrls: ['./password-generator.component.scss']
+})
+export class PasswordGeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
