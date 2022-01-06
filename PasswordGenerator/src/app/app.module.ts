985d7fb32e9dcab6ff4111ace0b02ada12984924
Fernando <ferenandoruiz@gmail.com>
2022-01-06 23:29:37 +0100

    All funcionality added

--- PasswordGenerator/src/app/app.module.ts
+++ PasswordGenerator/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PasswordGeneratorComponent } from './components/password-generator/password-generator.component';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
+import { ClipboardModule } from 'ngx-clipboard';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { PasswordGeneratorComponent } from './components/password-generator/pass
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatSliderModule,
+    ClipboardModule,
+    MatTooltipModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- PasswordGenerator/src/app/components/password-generator/password-generator.component.ts
+++ PasswordGenerator/src/app/components/password-generator/password-generator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-password-generator',
@@ -7,9 +8,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PasswordGeneratorComponent implements OnInit {
 
-  constructor() { }
+  constructor(private fb : FormBuilder) { }
 
   ngOnInit(): void {
+    this.options = this.fb.group({
+      'symbols' : true,
+      'numbers' : true,
+      'lowercase' : true,
+      'uppercase' : true
+    });
+    
+    this.options.valueChanges.subscribe( (options) => {
+      this.fullarray = []
+
+      for (let option in options) {
+        if (options[option]) {
+          if (option === 'symbols') {
+            this.fullarray = this.fullarray.concat(this.symbols);
+          }else if (option === 'numbers') {
+            this.fullarray = this.fullarray.concat(this.numbers);
+          }else if (option === 'uppercase') {
+            this.fullarray = this.fullarray.concat(this.uppercase);
+          }else if (option === 'lowercase') {
+            this.fullarray = this.fullarray.concat(this.lowercase);
+          }
+        }
+      }
+      this.generatePassword(this.length);
+    })    
   }
 
+  options : FormGroup = new FormGroup({})
+
+  symbols = this.arrayFromLowToHigh(33, 47).concat(this.arrayFromLowToHigh(58, 64)).concat(this.arrayFromLowToHigh(91, 96)).concat(this.arrayFromLowToHigh(123, 126));
+  numbers = this.arrayFromLowToHigh(48, 57);
+  uppercase = this.arrayFromLowToHigh(65, 90);
+  lowercase = this.arrayFromLowToHigh(97, 122);
+  fullarray : any[] = this.symbols.concat(this.numbers).concat(this.uppercase).concat(this.lowercase);
+  length : number = 0;
+  password : string = '';
+  copied : boolean = false;
+
+  arrayFromLowToHigh(low : number, high : number) {
+    let array = [];
+    for (let i = low; i <= high; i++) {
+      array.push(i);
+    }
+    return array;
+  }
+
+  changeLength() {
+    this.generatePassword(this.length);
+  }
+
+  generatePassword(length : number) {
+    let password = '';
+    for(let i = 0; i < length; i++) {
+      let ascii = this.fullarray[Math.floor(Math.random() * this.fullarray.length)];
+      if (ascii != undefined) {
+        password += String.fromCharCode(ascii);
+      }
+      
+    }
+    this.password = password.replace('', ' ');
+  }
 }
